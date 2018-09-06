053b6897de98348e01c8881b2d71d1da28393e0b
Konrad <kjimene@keplergrp.com>
2018-09-06 19:06:06 -0400

    added new tables

--- flask/flaskr/schema.sql
+++ flask/flaskr/schema.sql
@@ -17,12 +17,11 @@ CREATE TABLE IF NOT EXISTS trivia (
 
 CREATE TABLE IF NOT EXISTS question(
   id INTEGER NOT NULL,
-  trivia INTEGER NOT NULL,
+  trivia INTEGER NOT NUL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
   FOREIGN KEY (trivia) REFERENCES trivia(id)
 );
-
 
 CREATE TABLE IF NOT EXISTS question_option(
   id INTEGER NOT NULL,
@@ -32,4 +31,21 @@ CREATE TABLE IF NOT EXISTS question_option(
   content TEXT,
   FOREIGH KEY (trivia, question) REFERENCES question(trivia, id)
 );
+
+CREATE TABLE IF NOT EXISTS trivia_response (
+  trivia INTEGER NOT NULL,
+  user INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP.:
+);
+
+CREATE TABLE IF NOT EXISTS option_reponse (
+  user INTEGER NOT NULL,
+  question INTEGER NOT NULL,
+  option INTEGER NOT NULL
+
+);
+
+
+
+
 
